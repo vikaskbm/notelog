1ee89f755c72c787c659ae484126e37a4a30c4a6
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-15 04:15:13 +0530

    import all packages and functions

---
+++ src/sidebar/sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import { Divider, Button } from "@material-ui/core";
+import styles from "./styles";
+import SidebarItemComponent from "../sidebaritem/sidebarItem";
+
+const SideBarComponent = () => {
+  return <h1>Hello from sidebar component</h1>;
+};
+
+export default SideBarComponent;
---
+++ src/sidebarItem/sidebarItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import DeleteIcon from "@material-ui/icons/Delete";
+import styles from "./styles";
+import { removeHTMLTags } from "../helpers";
+
+const SideBarItemComponent = () => {
+  return <h1>Hello from sidebar-item component</h1>;
+};
+
+export default SideBarItemComponent;
