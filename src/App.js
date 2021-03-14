1e5c7ecdbb0c8d9f8e7c4923e14d658fc7abf69b
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-15 03:40:14 +0530

    log note from firebase on render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
+import firebase from "firebase";
 
 function App() {
+  const [state, setState] = useState({
+    selectedNoteIndex: null,
+    selectedNote: null,
+    notes: null,
+  });
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .onSnapshot((serverUpdate) => {
+        const notes = serverUpdate.docs.map((doc) => {
+          const data = doc.data();
+          data["id"] = doc.id;
+          return data;
+        });
+        console.log(notes);
+        setState({ notes: notes });
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello world</h1>
