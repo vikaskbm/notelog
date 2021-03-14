e05ce93886a3b524bef64bfe504eb0db57bf6e1e
Vikas Bishnoi <vikaskbm@gmail.com>
2021-03-15 02:49:05 +0530

    initialize firebase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAIONWZRo0WMQf9z_dT85Z_ZQbIY3fz2so",
+  authDomain: "notelog-96783.firebaseapp.com",
+  projectId: "notelog-96783",
+  storageBucket: "notelog-96783.appspot.com",
+  messagingSenderId: "1071396361608",
+  appId: "1:1071396361608:web:a7e73323b346ac4bf2563d",
+  measurementId: "G-4XBZW6XS5Z",
+});
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
